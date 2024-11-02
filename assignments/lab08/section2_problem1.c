cbe248f4aab0dbc6b98c7106270e58ab8eef9017
Muhammad Affan Rasheed <affanrasheed2751@gmail.com>
2024-11-02 23:07:02 +0500

    Update section2_problem1.c

--- assignments/lab08/section2_problem1.c
+++ assignments/lab08/section2_problem1.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-
-int main() {
-    int three_d_arr[2][3][3] = {
-        {{21, 13, 33}, {23, 34, 15},{23, 42, 27}},
-        {{51, 62, 83}, {74, 85, 80},{12, 39, 43}}
-    };
-
-    int sum1 = 0, sum2 = 0;
-    for (int i = 0; i < 1; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 3; k++) {
-                sum1 += three_d_arr[i][j][k];
-            }
-        }
+int main(){
+    int rows,columns,i,j;
+    printf("enter rows for matrix: ");
+    scanf("%d",&rows);
+    printf("enter columns for matrix: ");
+    scanf("%d",&columns);
+    int matrix[rows][columns],transpose[columns][rows];
+    for ( i = 0; i < rows; i++)
+    {
+       for ( j = 0; j < columns; j++)
+       {
+        printf("enter value for row %d and column %d: ",i+1,j+1);
+        scanf("%d",&matrix[i][j]);
+       }
+       
     }
-    for (int i = 1; i < 2; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 3; k++) {
-                sum2 += three_d_arr[i][j][k];
-            }
-        }
+    for ( i = 0; i < columns; i++)
+    {
+       for ( j = 0; j < rows; j++)
+       {
+        transpose[i][j] = matrix[j][i];
+       }
     }
-    
-    printf("sum of all elements on page 1 is: %d",sum1);
-    printf("\nsum of all elements on page 2 is: %d",sum2);
+    printf("transpose is: \n");
+    for ( i = 0; i < columns; i++)
+    {
+       for ( j = 0; j < rows; j++)
+       {
+        printf("%d ",transpose[i][j]);
+       }
+       printf("\n");
+    }
     return 0;
+    
 }
